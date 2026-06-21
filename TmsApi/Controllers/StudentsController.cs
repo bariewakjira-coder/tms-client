e91f3347a5d029cf5eb018d86a60de0f11e8115f
Tamiru Asbe <tamiruasbe10@gmail.com>
2026-06-21 12:59:41 +0300

    module-5 session-2

---
+++ TmsApi/Controllers/StudentsController.cs
@@ -0,0 +1,53 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TmsApi.Data;
+
+namespace TmsApi.Controllers;
+
+[ApiController]
+[Route("api/students")]
+public class StudentsController : ControllerBase
+{
+    private readonly TmsDbContext context;
+
+    public StudentsController(TmsDbContext context)
+    {
+        this.context = context;
+    }
+
+
+    [HttpGet]
+    public async Task<IActionResult> GetStudents(
+        int page = 1,
+        CancellationToken cancellationToken = default)
+    {
+        int pageSize = 20;
+
+
+        var students = await context.Students
+            .OrderBy(s => s.Name)   // IMPORTANT FIRST
+            .Skip((page - 1) * pageSize)
+            .Take(pageSize)
+            .ToListAsync(cancellationToken);
+
+
+        return Ok(students);
+    }
+
+    [HttpGet("top-courses")]
+public async Task<IActionResult> GetTopCourses()
+{
+    var courses = await context.Courses
+        .Select(c => new
+        {
+            c.Title,
+            EnrollmentCount = c.Enrollments.Count()
+        })
+        .OrderByDescending(x => x.EnrollmentCount)
+        .Take(5)
+        .ToListAsync();
+
+
+    return Ok(courses);
+}
+}
